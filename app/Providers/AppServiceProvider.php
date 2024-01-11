7754ad3b0e74a86a77746432ee6630b08da95314
Gerhard <xgerhard@2g.be>
2024-01-11 13:44:04 +0100

    cleanup  sanctum migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        \Laravel\Sanctum\Sanctum::ignoreMigrations();
     }
 
     /**
